c5152c96ff53921042cbe067d669326f29129499
vincentzyc <vincentzyc@qq.com>
2022-09-21 11:25:44 +0800

    🎉 init: init

---
+++ src/components/Agreement/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <van-checkbox v-model="checked" icon-size="14px" checked-color="#666666" label-disabled>
+    <p class="agreement-text">
+      <span>{{ title }}</span>
+      <span v-for="(agr, key) in agrList">
+        <span v-if="key > 0">和</span>
+        <span class="agreement-title" @click.stop="openAgreement(agr.title, agr.text)">{{ agr.title }}</span>
+      </span>
+    </p>
+  </van-checkbox>
+  <AgreementPopup v-model:show="showPopup" :text="curText" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  agrList: [
+    {
+      title: string;
+      text: string;
+    }
+  ];
+}>();
+let checked = $ref(false),
+  showPopup = $ref(false),
+  curText = $ref("");
+
+function openAgreement(title: string, text: string) {
+  showPopup = true;
+  curText = text;
+}
+</script>
---
+++ src/components/AgreementPopup/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <van-popup v-model:show="show" class="agreement-popup" style="max-width: 576px" teleport="body">
+    <div class="agreement-wrap">
+      <iframe :src="text" frameborder="0" style="width: 100%; height: 100%"></iframe>
+    </div>
+    <div class="agreement-confirm" @click="close()">我知道了</div>
+  </van-popup>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  show: boolean;
+  text: string;
+}>();
+const emit = defineEmits<{
+  (e: "update:show", bool: boolean): void;
+}>();
+function close() {
+  emit("update:show", false);
+}
+</script>
