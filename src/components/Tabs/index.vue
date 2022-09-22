0effd2b20215700cb8747b99eec450f54e09f279
vincentzyc <vincentzyc@qq.com>
2022-09-22 09:24:32 +0800

    🎉 init: init

---
+++ src/components/Tabs/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wg-tabs clearfix">
+    <ul class="flex tab-bar-wrap align-end">
+      <li
+        class="flex flex-center tab-bar"
+        v-for="(item, key) in tabList"
+        :class="{ active: activeTab === key }"
+        @click="changeTab(key)"
+      >
+        <span class="tab-bar-title">{{ item.title }}</span>
+        <img v-lazy="item.logo" :alt="item.title" class="tab-bar-logo" />
+      </li>
+    </ul>
+    <div v-if="tabList[0].loaded" v-show="activeTab === 0" class="tab-content">
+      <Tab1 />
+    </div>
+    <div v-if="tabList[1].loaded" v-show="activeTab === 1" class="tab-content">
+      <Tab2 />
+    </div>
+    <div v-if="tabList[2].loaded" v-show="activeTab === 2" class="tab-content">
+      <Tab3 />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import tablogo1 from "@/assets/img/tablogo1.png";
+import tablogo2 from "@/assets/img/tablogo2.png";
+import tablogo3 from "@/assets/img/tablogo3.png";
+// defineProps<{ msg: string }>();
+
+let tabList = $ref([
+  {
+    title: "联通",
+    logo: tablogo1,
+    loaded: true,
+  },
+  {
+    title: "电信",
+    logo: tablogo2,
+    loaded: false,
+  },
+  {
+    title: "移动",
+    logo: tablogo3,
+    loaded: false,
+  },
+]);
+
+let activeTab = $ref(0);
+
+function changeTab(i: number) {
+  const tabItem = tabList[i];
+  tabItem.loaded = true;
+  activeTab = i;
+}
+</script>
