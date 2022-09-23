0ea975a58d8bf5ac834312c433f985ea39d594f5
vincentzyc <vincentzyc@qq.com>
2022-09-23 18:23:20 +0800

    🎉 init: init

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
@@ -15,6 +16,14 @@ export default defineConfig({
   plugins: [
     vue({
       reactivityTransform: true
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/, /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue'],
     }),
     Components({
       resolvers: [VantResolver()],
