03abc2f3f07ed6dc05ca55fe73dc62fb483d0b2e
vincentzyc <vincentzyc@qq.com>
2022-09-24 12:23:13 +0800

    🎉 init: init

--- src/components/Tab3/Tab3.vue
+++ src/components/Tab3/Tab3.vue
@@ -7,8 +7,9 @@
   <div class="text-center expenses-wrap">
     <span class="expenses-text" @click="showExpenses()">资费详情说明</span>
   </div>
-  <van-popup class="center-popup" v-model:show="show" round>
-    <div class="popup-wrap">{{ expensesText }}</div>
+  <van-popup class="agreement-popup" v-model:show="show" round>
+    <div class="agreement-wrap">{{ expensesText }}</div>
+    <div class="agreement-confirm" @click="close()">我知道了</div>
   </van-popup>
 </template>
 
@@ -35,5 +36,8 @@ let show = $ref(false),
   ]);
 function showExpenses() {
   show = true;
+}
+function close() {
+  show = false;
 }
 </script>
