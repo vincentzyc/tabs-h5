19fede858ce4ad861cf0c836cb80cb09191330d4
vincentzyc <vincentzyc@qq.com>
2022-09-25 10:59:58 +0800

    🎉 init: init

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
+import legacy from '@vitejs/plugin-legacy'
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
@@ -28,6 +29,9 @@ export default defineConfig({
     Components({
       resolvers: [VantResolver()],
     }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
   ],
   test: {
     globals: true,
