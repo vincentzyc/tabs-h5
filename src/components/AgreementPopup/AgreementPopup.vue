972c526c958d2a123e221d0dea44a1b79de9b840
vincentzyc <vincentzyc@qq.com>
2022-09-26 12:33:20 +0800

    🎉 init: init

--- src/components/AgreementPopup/AgreementPopup.vue
+++ src/components/AgreementPopup/AgreementPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <van-popup v-model:show="show" class="agreement-popup" style="max-width: 576px" teleport="body">
+  <van-popup v-model:show="show" class="agreement-popup" style="max-width: 576px" teleport="body" @closed="close">
     <div class="agreement-wrap">
       <iframe :src="text" frameborder="0" style="width: 100%; height: 100%"></iframe>
     </div>
