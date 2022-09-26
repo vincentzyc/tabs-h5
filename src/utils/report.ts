a99f4b548a7d11ea9b70de5873de6777a1d69ec7
vincentzyc <vincentzyc@qq.com>
2022-09-26 12:33:36 +0800

    🎉 init: init

--- src/utils/report.ts
+++ src/utils/report.ts
@@ -36,3 +36,10 @@ export function reportMatomo(title: string, value = '') {
   pid ? window._paq.push(["trackEvent", `${document.title}-${templateId}-${pid}`, title, value])
     : window._paq.push(["trackEvent", `${document.title}-${templateId}`, title, value]);
 }
+
+export function initfingerprint2() {
+  if (typeof murmur !== 'undefined' && murmur) return
+  let script = document.createElement("script");
+  script.src = 'https://cdn.lipush.com/other/js/fingerprint2@1.0.1.js';
+  document.head.appendChild(script);
+}
