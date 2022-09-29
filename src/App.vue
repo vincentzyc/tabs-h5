206d8a14cc1889c43f3b7b8c3b168b5a4946b7d9
vincentzyc <vincentzyc@qq.com>
2022-09-29 15:08:39 +0800

    🎉 init: init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Home from "@/pages/Home.vue";
-import { reportMatomo } from "./utils/report";
-window.onload = () => {
-  let PAGESTARTTIME = performance.timing.fetchStart;
-  const curTime = new Date().getTime();
-  reportMatomo("首屏加载时长", (curTime - PAGESTARTTIME) / 1000 + "s");
-};
-</script>
-
 <template>
-  <Home />
+  <component :is="curPage"></component>
 </template>
+
+<script lang="ts">
+import RouterComponent from "@/router";
+import { getUrlParam } from "./utils";
+import { reportMatomo } from "./utils/report";
+
+export default defineComponent({
+  name: "App",
+  components: RouterComponent,
+  setup() {
+    window.onload = () => {
+      let PAGESTARTTIME = performance.timing.fetchStart;
+      const curTime = new Date().getTime();
+      reportMatomo("首屏加载时长", (curTime - PAGESTARTTIME) / 1000 + "s");
+    };
+
+    function getPage() {
+      let page = getUrlParam("id");
+      if (page === undefined || page === null) {
+        page = "202209271020";
+      } else if (typeof page === "string") {
+        if (!Object.prototype.hasOwnProperty.call(RouterComponent, page)) page = "404";
+      } else {
+        page = "404";
+      }
+      return page;
+    }
+    let curPage = getPage();
+
+    return {
+      curPage,
+    };
+  },
+});
+</script>
