d209096732e2884ed2e74c0968aa2157058400b3
vincentzyc <vincentzyc@qq.com>
2022-10-03 08:46:58 +0800

    🎉 init: init

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 export default {
-  "202209271020": defineAsyncComponent(() => import(`@/pages/202209271020.vue` /* webpackChunkName: "[request]" */)),
-  "202209291430": defineAsyncComponent(() => import(`@/pages/202209291430.vue` /* webpackChunkName: "[request]" */)),
+  "page1": defineAsyncComponent(() => import(`@/pages/page1.vue` /* webpackChunkName: "[request]" */)),
+  "page2": defineAsyncComponent(() => import(`@/pages/page2.vue` /* webpackChunkName: "[request]" */)),
   "404": defineAsyncComponent(() => import(`@/pages/404.vue` /* webpackChunkName: "[request]" */)),
 };
