d9675d1a56e0f3230a3202059b69a45c5d96809b
vincentzyc <vincentzyc@qq.com>
2022-10-05 17:04:22 +0800

    🎉 init: init

--- src/components/Tab3/Tab3.vue
+++ src/components/Tab3/Tab3.vue
@@ -3,6 +3,10 @@
   <MarqueeSingle class="mg-b10" />
   <img src="@/assets/img/tab3-2.jpg" alt="产品说明" class="widthfull" />
   <img src="@/assets/img/tab3-3.jpg" alt="产品说明" class="widthfull" />
+  <p class="fs12 cccc lh20" @click="showTipPopup()">
+    温馨提醒：
+    个人擅自买卖实名电话卡属违法行为，请勿将号卡租借、贩卖给他人，如被他人利用发生非法违规行为，您将承担相应法律责任，请您确保规范使用您的号码。查看详情>>>
+  </p>
   <img src="@/assets/img/tab3-4.png" alt="产品说明" class="widthfull" />
   <div class="text-center expenses-wrap">
     <span class="expenses-text" @click="showExpenses()">资费详情说明</span>
@@ -14,13 +18,18 @@
       >
       <p class="info-text">根据国家手机号卡实名制规定</p>
       <p class="info-text">请如实填写信息，以便我们及时为您送达</p>
-      <BaseForm :handleNo="selectNumItem.num" @submit="submitOrder" />
+      <BaseForm :handleNo="selectNumItem.num" @submit="submitOrder" animteBtn/>
       <Agreement title="填写并提交视为阅读并同意" :agrList="agrList" v-model:checked="checked" />
     </div>
   </van-popup>
   <van-popup class="agreement-popup" v-model:show="show" round>
     <div class="agreement-wrap">{{ expensesText }}</div>
     <div class="agreement-confirm" @click="close()">我知道了</div>
+  </van-popup>
+  <van-popup v-model:show="showTip" class="common-popup" style="max-width: 576px" teleport="body">
+    <div class="common-wrap">
+      <img src="@/assets/img/tab3-5.jpg" alt="温馨提醒" class="widthfull" />
+    </div>
   </van-popup>
 </template>
 
@@ -32,6 +41,7 @@ provide("tabIndex", 2);
 
 let showForm = $ref(false),
   show = $ref(false),
+  showTip = $ref(false),
   checked = $ref(true),
   agrList = $ref([
     {
@@ -67,7 +77,9 @@ function showExpenses() {
 function close() {
   show = false;
 }
-
+function showTipPopup() {
+  showTip = true;
+}
 function selectNum(numItem: TypeNumItem, selectedBelong: string[]) {
   selectNumItem = numItem;
   showCity = selectedBelong.join(" ");
