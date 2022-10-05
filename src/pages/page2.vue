e34b20089f0687b9433bfeaa0a827e986a47ba56
vincentzyc <vincentzyc@qq.com>
2022-10-05 17:03:45 +0800

    🎉 init: init

---
+++ src/pages/page2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <TabsPage2 />
+    <img alt="领卡流程" class="widthfull" v-lazy="img2" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import img2 from "@/assets/img/img2.jpg";
+import { CommonApi } from "@/api";
+import { getUrlParam } from "@/utils";
+import { useMainStore } from "@/pinia";
+const mainStore = useMainStore();
+
+const pid = getUrlParam("pid");
+
+async function getBaseData() {
+  if (!pid) return;
+  mainStore.setPid(pid);
+  const param = { pid: pid, url: window.location.href, ipLocation: "1" };
+  let res = await CommonApi.pageIdLocation(param);
+  mainStore.setCjAllData(res);
+}
+getBaseData();
+</script>
