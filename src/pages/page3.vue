c889056786f046ff404da06544d69b4cc9fe77f5
vincentzyc <vincentzyc@qq.com>
2022-10-09 12:30:46 +0800

    🎉 init: init

---
+++ src/pages/page3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper flex flex-center" style="user-select: none">
+    <DragVerify
+      v-model:isPassing="isPassing2"
+      text="请按住滑块拖动"
+      successText="验证通过"
+      handlerIcon="el-icon-d-arrow-right"
+      successIcon="el-icon-circle-check"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+let isPassing2 = $ref(false);
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 export default {
   "page1": defineAsyncComponent(() => import(`@/pages/page1.vue` /* webpackChunkName: "[request]" */)),
   "page2": defineAsyncComponent(() => import(`@/pages/page2.vue` /* webpackChunkName: "[request]" */)),
+  "page3": defineAsyncComponent(() => import(`@/pages/page3.vue` /* webpackChunkName: "[request]" */)),
   "404": defineAsyncComponent(() => import(`@/pages/404.vue` /* webpackChunkName: "[request]" */)),
 };
