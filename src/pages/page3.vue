5789078d6cb21583a5688b0ea5dd3bbf9cf3d1ca
vincentzyc <vincentzyc@qq.com>
2022-10-10 11:29:27 +0800

    🎉 init: init

--- src/pages/page3.vue
+++ src/pages/page3.vue
@@ -2,8 +2,6 @@
   <div class="wrapper flex flex-center" style="user-select: none">
     <DragVerify
       v-model:isPassing="isPassing2"
-      text="请按住滑块拖动"
-      successText="验证通过"
       handlerIcon="el-icon-d-arrow-right"
       successIcon="el-icon-circle-check"
     />
