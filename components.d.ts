fd7ae2003ff68399fc9c5c6e47a09e91c6464487
vincentzyc <vincentzyc@qq.com>
2022-10-30 01:39:17 +0800

    🧪 test: add vitest

--- components.d.ts
+++ components.d.ts
@@ -20,6 +20,7 @@ declare module '@vue/runtime-core' {
     FormIDCard: typeof import('./src/components/FormItem/FormIDCard.vue')['default']
     FormName: typeof import('./src/components/FormItem/FormName.vue')['default']
     FormPhone: typeof import('./src/components/FormItem/FormPhone.vue')['default']
+    Hello: typeof import('./src/components/Hello.vue')['default']
     MarqueeSingle: typeof import('./src/components/MarqueeSingle/MarqueeSingle.vue')['default']
     NumberRule: typeof import('./src/components/NumberRule/NumberRule.vue')['default']
     PhoneList: typeof import('./src/components/PhoneList/PhoneList.vue')['default']
