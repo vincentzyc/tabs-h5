f6c3ccfcf1b1c37b734b6db13dda18ecec4dfe1d
vincentzyc <vincentzyc@qq.com>
2022-11-06 09:59:26 +0800

    🧪 test: add test

--- src/test/FormName.test.ts
+++ src/test/FormName.test.ts
@@ -4,7 +4,17 @@ import FormName from '@/components/FormItem/FormName.vue'
 test('mount FormName component', async () => {
   expect(FormName).toBeTruthy()
 
-  const wrapper = mount(FormName)
+  const wrapper = mount(FormName, {
+    props: {
+      modelValue: ''
+    }
+  })
 
   expect(wrapper.html()).toMatchSnapshot()
+
+  await wrapper.get('input').setValue('测试姓名')
+
+  const emitEvent = wrapper.emitted('update:modelValue')
+
+  expect(emitEvent).toHaveLength(1)
 })
