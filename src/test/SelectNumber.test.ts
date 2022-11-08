009c6c1dd595d81ea5d5d4decab7924bd5597230
vincentzyc <vincentzyc@qq.com>
2022-11-08 10:25:33 +0800

    🧪 test: add test

--- src/test/SelectNumber.test.ts
+++ src/test/SelectNumber.test.ts
@@ -2,29 +2,17 @@ import { mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import SelectNumber from '@/components/SelectNumber/SelectNumber.vue'
 
-// function sleep(delay = 0): Promise<void> {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, delay);
-//   });
-// }
-
 test('mount SelectNumber component', async () => {
   expect(SelectNumber).toBeTruthy()
 
   const wrapper = mount(SelectNumber, {
     global: {
       plugins: [createTestingPinia()],
-    },
-    props: {
-
+      provide: {
+        tabIndex: 0
+      }
     },
   })
 
   expect(wrapper.html()).toMatchSnapshot()
-
-  // await wrapper.get('.agreement-confirm').trigger('click')
-
-  // const emitEvent = wrapper.emitted('update:show')
-
-  // expect(emitEvent).toHaveLength(1)
 })
