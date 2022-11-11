5159279e4561691d9bb30c29f35f29ed40f6bdae
vincentzyc <vincentzyc@qq.com>
2022-11-11 17:19:35 +0800

    🧪 test: add test

--- src/utils/__tests__/validate/email.spec.ts
+++ src/utils/__tests__/validate/email.spec.ts
@@ -2,7 +2,7 @@ import { isEmail } from '../../validate/email'
 
 test('isEmail', () => {
   expect(isEmail('abc@gmail.com')).toBeTruthy();
-  expect(isEmail(' abc@gmail.com ')).toBeTruthy();
+  expect(isEmail(' abc@gmail.com ')).toBeFalsy();
   expect(isEmail('abc@@gmail.com')).toBeFalsy();
   expect(isEmail('@gmail.com')).toBeFalsy();
   expect(isEmail('aa@bb.cc')).toBeTruthy();
