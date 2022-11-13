57a140760b64de3190ad276147b848738bfeafa3
vincentzyc <vincentzyc@qq.com>
2022-11-13 11:30:11 +0800

    🧪 test: add test

---
+++ src/test/Tab3.test.ts
@@ -0,0 +1,20 @@
+
+
+import { mount } from '@vue/test-utils'
+import { Lazyload } from 'vant';
+import { createTestingPinia } from '@pinia/testing'
+import Tab3 from '@/components/Tab3/Tab3.vue'
+
+test('mount Tab3 component', async () => {
+  expect(Tab3).toBeTruthy()
+
+  const wrapper = mount(Tab3, {
+    global: {
+      plugins: [createTestingPinia()],
+      directives: {
+        lazy: Lazyload
+      }
+    },
+  })
+  expect(wrapper.html()).toMatchSnapshot()
+})
