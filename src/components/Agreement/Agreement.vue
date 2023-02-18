abbc2328477e25a1b0a848a70a96d56044f653b5
vincentzyc <vincentzyc@qq.com>
2023-02-18 20:11:52 +0800

    ⚡ perf: 代码优化

--- src/components/Agreement/Agreement.vue
+++ src/components/Agreement/Agreement.vue
@@ -1,12 +1,5 @@
 <template>
-  <van-checkbox
-    v-model="checked"
-    icon-size="14px"
-    checked-color="#cccccc"
-    label-disabled
-    @change="handleChecked"
-    @click="handleClick"
-  >
+  <van-checkbox v-model="isChecked" icon-size="14px" checked-color="#cccccc" label-disabled @click="handleClick">
     <p class="agreement-text">
       <span>{{ title }}</span>
       <span v-for="(agr, key) in agrList">
@@ -30,9 +23,14 @@ const props = defineProps<{
   }[];
 }>();
 
-const emit = defineEmits<{
+const emits = defineEmits<{
   (e: "update:checked", bool: boolean): void;
 }>();
+
+const isChecked = $computed({
+  get: () => props.checked,
+  set: v => emits("update:checked", v),
+});
 
 let showPopup = $ref(false),
   curText = $ref("");
@@ -40,9 +38,6 @@ let showPopup = $ref(false),
 function openAgreement(title: string, text: string) {
   showPopup = true;
   curText = text;
-}
-function handleChecked(bool: boolean) {
-  emit("update:checked", bool);
 }
 function handleClick() {
   props.checked ? reportMatomo("勾选协议") : reportMatomo("取消勾选协议");
