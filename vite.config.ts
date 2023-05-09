b866e057df2c06f3aa1c518ae741afe65f737434
vincentzyc <vincentzyc@qq.com>
2023-05-09 09:44:07 +0800

    ✅test: sentry test

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,14 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
 import legacy from '@vitejs/plugin-legacy'
 import { VantResolver } from 'unplugin-vue-components/resolvers';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
   server: {
     host: true,
     port: 5100
@@ -31,7 +35,23 @@ export default defineConfig({
     }),
     legacy({
       targets: ['defaults', 'not IE 11']
-    })
+    }),
+    sentryVitePlugin({
+      org: "sentry",
+      project: "tabsh5",
+      authToken: '60d6e12135c14df8af9e97853fd1e53817129334af0241fc8ac140deed08be6c',
+      release: 'tabsh5@1.0.0',
+      deploy: {
+        env: 'production'
+      },
+      setCommits: {
+        auto: true
+      },
+      sourcemaps: {
+        assets: "./dist/assets/**",
+      },
+      url:"http://101.33.201.93:9000/"
+    }),
   ],
   test: {
     globals: true,
