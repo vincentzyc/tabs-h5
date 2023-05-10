df055b48c124dd9f0c86f120df57379bee0dbc28
vincentzyc <vincentzyc@qq.com>
2023-05-10 16:35:37 +0800

    ✅test: sentry test

--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -7,19 +7,29 @@
 </template>
 
 <script setup lang="ts">
-import img2 from "@/assets/img/img2.jpg";
-import { CommonApi } from "@/api";
-import { getUrlParam } from "@/utils";
-import { useMainStore } from "@/pinia";
+import img2 from '@/assets/img/img2.jpg';
+import { CommonApi } from '@/api';
+import { getUrlParam } from '@/utils';
+import { useMainStore } from '@/pinia';
+// import * as Sentry from '@sentry/vue';
+
 const mainStore = useMainStore();
 
-const pid = getUrlParam("pid");
+const pid = getUrlParam('pid');
 
 async function getBaseData() {
   if (!pid) return;
   mainStore.setPid(pid);
-  const param = { pid: pid, url: window.location.href, ipLocation: "1" };
+  const param = { pid: pid, url: window.location.href, ipLocation: '1' };
   let res = await CommonApi.pageIdLocation(param);
+
+  // Sentry.addBreadcrumb({
+  //   category: 'data',
+  //   message: 'pageIdLocation back data',
+  //   data: res,
+  //   level: 'info',
+  // });
+
   mainStore.setCjAllData(res);
 }
 getBaseData();
