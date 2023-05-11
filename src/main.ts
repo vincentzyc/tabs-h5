c1f40d8029334d1e7372b71cf28e8a5e053157d2
vincentzyc <vincentzyc@qq.com>
2023-05-11 09:29:38 +0800

    ✅test: sentry test

--- src/main.ts
+++ src/main.ts
@@ -29,15 +29,15 @@ Sentry.init({
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  beforeBreadcrumb(event,hint) {
-    if(event.category === "xhr"){
-      if(event.data) {
-        event.data.request = hint?.input
-        event.data.response = hint?.xhr.response
-      }
-    }
-    return event;
-  },
+  // beforeBreadcrumb(event,hint) {
+  //   if(event.category === "xhr"){
+  //     if(event.data) {
+  //       event.data.request = hint?.input
+  //       event.data.response = hint?.xhr.response
+  //     }
+  //   }
+  //   return event;
+  // },
 });
 
 app.use(Lazyload);
--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -11,7 +11,7 @@ import img2 from '@/assets/img/img2.jpg';
 import { CommonApi } from '@/api';
 import { getUrlParam } from '@/utils';
 import { useMainStore } from '@/pinia';
-// import * as Sentry from '@sentry/vue';
+import * as Sentry from '@sentry/vue';
 
 const mainStore = useMainStore();
 
@@ -23,12 +23,12 @@ async function getBaseData() {
   const param = { pid: pid, url: window.location.href, ipLocation: '1' };
   let res = await CommonApi.pageIdLocation(param);
 
-  // Sentry.addBreadcrumb({
-  //   category: 'data',
-  //   message: 'pageIdLocation back data',
-  //   data: res,
-  //   level: 'info',
-  // });
+  Sentry.addBreadcrumb({
+    category: 'data',
+    message: 'pageIdLocation back data',
+    data: res,
+    level: 'info',
+  });
 
   mainStore.setCjAllData(res);
 }
