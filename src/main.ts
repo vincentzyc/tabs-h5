5a65dfdda76bfcdbc4ff182a6837fb94a1b1a949
vincentzyc <vincentzyc@qq.com>
2023-05-18 17:12:01 +0800

    ✅test: sentry test

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ import 'vant/es/toast/style';
 import 'vant/es/dialog/style';
 import './assets/css/base.css';
 import './assets/css/main.css';
-import pkg from '../package.json';
+// import pkg from '../package.json';
 
-import * as Sentry from '@sentry/vue';
+// import * as Sentry from '@sentry/vue';
 
 const app = createApp(App);
 
@@ -18,26 +18,26 @@ app.use(createPinia());
 
 app.config.globalProperties.$axios = Axios;
 
-Sentry.init({
-  app,
-  dsn: 'https://1ef8fe45d8ce4fb1b7fae7071f9982fc@o1343443.ingest.sentry.io/6618212',
-  integrations: [new Sentry.Replay()],
-  release: pkg.version,
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  beforeBreadcrumb(event, hint) {
-    if (event.category === 'xhr') {
-      if (event.data) {
-        event.data.request = hint?.input ? JSON.parse(hint?.input) : '';
-        event.data.response = hint?.xhr.response ? JSON.parse(hint?.xhr.response) : '';
-      }
-    }
-    return event;
-  },
-});
+// Sentry.init({
+//   app,
+//   dsn: 'https://1ef8fe45d8ce4fb1b7fae7071f9982fc@o1343443.ingest.sentry.io/6618212',
+//   integrations: [new Sentry.Replay(), new Sentry.BrowserTracing()],
+//   release: pkg.version,
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+//   // Session Replay
+//   replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+//   beforeBreadcrumb(event, hint) {
+//     if (event.category === 'xhr') {
+//       if (event.data) {
+//         event.data.request = hint?.input ? JSON.parse(hint?.input) : '';
+//         event.data.response = hint?.xhr.response ? JSON.parse(hint?.xhr.response) : '';
+//       }
+//     }
+//     return event;
+//   },
+// });
 
 app.use(Lazyload);
 app.mount('#app');
